b8534b0338335cae77044abfa38754ebee2cc289
Danny Hieber <dwhieb@gmail.com>
2019-09-28 12:36:56 -0500

    write TSV file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
-function concordance() {
-  console.info(`Running concordancer`);
+const createCSVStream = require(`csv-stringify`);
+const { createWriteStream } = require(`fs`);
+
+const defaultColumns = [
+  `text`,
+  `utterance`,
+  `word`,
+  `token`,
+];
+
+function concordance(
+  wordforms = [],
+  dir = `.`,
+  outputPath = `concordance.tsv`
+) {
+
+  wordforms = Array.from(wordforms); // eslint-disable-line no-param-reassign
+
+  const csvOptions = {
+    columns:   defaultColumns,
+    delimiter: `\t`,
+    header:    true,
+    quote:     ``,
+  };
+
+  const csvStream   = createCSVStream(csvOptions);
+  const writeStream = createWriteStream(outputPath, `utf8`);
+
+  csvStream
+  .on(`error`, console.error)
+  .pipe(writeStream)
+  .on(`error`, console.error);
+
+  csvStream.end();
+
 }
 
 module.exports = concordance;
